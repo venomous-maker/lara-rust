e85870230c424e6e4a80b99067d2749a062d5eac
venomous-maker <morganokumu8+1@gmail.com>
2026-06-03 17:12:31 +0300

    feat(core): export env helpers from crate root

--- crates/lara-core/src/lib.rs
+++ crates/lara-core/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod application;
 pub mod config;
 pub mod container;
+pub mod env;
 pub mod error;
 pub mod service_provider;
 
 pub use application::Application;
 pub use config::Config;
+pub use env::{env, env_bool, env_or, env_or_parse, env_parse};
 pub use container::{Container, SharedContainer, make_container};
 pub use error::{CoreError, Result};
 pub use service_provider::{ServiceProvider, ProviderRegistry};
